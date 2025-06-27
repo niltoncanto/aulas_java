5ed7aebc189b352256a2ce7d4667387b050b95a5
Nilton Canto <nilton.canto@gmail.com>
2025-06-27 10:59:47 -0300

    novos exercícios

---
+++ clinica_springboot/clinica/src/test/java/com/mack/clinica/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mack.clinica.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
